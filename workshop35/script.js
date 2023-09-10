5f6733ab3ebf4d1d4a99e34c26f447213f76f477
Alisa Bielenko <alisabelenko1309@gmail.com>
2023-09-10 23:19:20 +0200

    lesson_35

---
+++ workshop35/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    getAllPublic();
+});
+
+let publicHTML = document.querySelector('.public');
+
+function getAllPublic() {
+    fetch("https://gorest.co.in/public/v2/users").then(response => {
+        return response.json();
+    })
+    .then((data) => {
+        data.forEach((element) => {
+            publicHTML.innerHTML += `
+            <div class="card-body public">
+                <h5 class="card-title name">${element.name}</h5>
+                <p class="card-text email">email: ${element.email}</p>
+                <p class="card-text gender">gender: ${element.gender}</p>
+                <p class="card-text status">status: ${element.status}</p>
+                <a href="index1.html" class="btn btn-primary" onclick="getContent('${element.id}')">Open all posts of this user</a>
+            </div>
+            `;
+        });
+    });
+}
+
+function getContent(id) {
+    localStorage.setItem('userId', id);
+}
