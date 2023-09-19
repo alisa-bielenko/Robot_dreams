5f81a60d821744d29acba3fb7754a1a4748c88d3
Alisa Bielenko <alisabelenko1309@gmail.com>
2023-09-19 12:35:42 +0200

    img isn't in components

--- Lesson_37/first-react-project/src/components/Products.jsx
+++ Lesson_37/first-react-project/src/components/Products.jsx
@@ -9,19 +9,19 @@ function Products() {
       name: 'Massage',
       price: 100,
       description: 'relaxing and sport',
-      imageUrl: 'src/components/img/relaxing.jpg' 
+      imageUrl: 'src/img/relaxing.jpg' 
     },
     { id: 2,
       name: 'Sauna',
       price: 150,
       description: 'dry/wet with swimming pool',
-      imageUrl: 'src/components/img/sauna.jpg'
+      imageUrl: 'src/img/sauna.jpg'
     },
     { id: 3,
       name: 'Cosmetic masks',
       price: 200,
       description: 'for face and body',
-      imageUrl: 'src/components/img/face.jpg' 
+      imageUrl: 'src/img/face.jpg' 
     }
   ];
 
