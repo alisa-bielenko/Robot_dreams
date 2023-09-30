0b6f158e8869480d24f94199e6db3efe0e88952a
Alisa Bielenko <alisabelenko1309@gmail.com>
2023-10-01 01:01:06 +0200

    lesson40

--- Lesson_40/project/src/CreateProduct/CreateProduct.jsx
+++ Lesson_40/project/src/CreateProduct/CreateProduct.jsx
@@ -14,23 +14,23 @@ export default function CreateProduct (props) {
         props.addNewProduct(data);
     };
 
-    return(
+    return (
         <div className='form-wrap'>
           <form className='form-box' onSubmit={handleSubmit(onSubmit)}>
             <div className='mb-3'>
-                <label htmlFor="title" className='form-label'>Name of product</label>
+                <label htmlFor='title' className='form-label'>Name of product</label>
                 <input id='title' className='form-control' {... register( "title", {required: 'Product name not entered'} )} />
                 {errors.title && (<div className='errors'>{errors.title.message}</div>)} 
             </div>
             <div className='mb-3' >
-                <label htmlFor="price" className='form-label'>Price of product</label>
+                <label htmlFor='price' className='form-label'>Price of product</label>
                 <input type='number' id='price' className='form-control' { ... register( "price", {required: 'Enter price of this product'} ) } /> 
                 {errors.price && (<div className='errors'>{errors.price.message}</div>)} 
             </div>
             <div className='mb-3'>
                 <input type='file' className='form-control' { ... register ( "thumbnail" ) } /> 
             </div>
-            <input type="submit" value="Send"/>
+            <input type='submit' value="Send"/>
           </form>  
         </div>
     );
